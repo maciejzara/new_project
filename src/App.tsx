c059bf4ac01983ad9188653e49433b617ab8ac72
Maciek <maciej.zarakowski@gmail.com>
2023-04-14 14:17:09 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> New Project - Map - git test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
