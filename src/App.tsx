4dfb1db5abfed784e73cfb5a55df262af7a50537
Maciek <maciej.zarakowski@gmail.com>
2023-04-27 14:51:31 +0200

    new Table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Form } from "./components/Form";
+import { Table } from "./components/Table";
 
 function App() {
+  const [levels, setLevels] = useState([]);
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+
+  const [locations, setLocations] = useState([]);
+  const [longitude, setLongitude] = useState("");
+  const [latitude, setLatitude] = useState("");
+  const [contitnet, setContinent] = useState("");
+  const [country, setCountry] = useState("");
+
+  // Która opcja jest lepsza ? Czy może useRef?
+  // const [levels, setLevels] = useState({name:"", description: ""});
+  // const [locations, setLocations] = useState({longitude: "", latitude: "", continent: "", country: ""});
+
   return (
     <div className="App">
       <header className="App-header">
-        <Form />
+        <Form
+          levels={levels}
+          setLevels={setLevels}
+          setName={setName}
+          setDescription={setDescription}
+          setLongitude={setLongitude}
+          setLatitude={setLatitude}
+          setContinent={setContinent}
+          setCountry={setCountry}
+          locations={locations}
+          setLocations={setLocations}
+        />
+        <Table
+          levels={levels}
+          setLevels={setLevels}
+          locations={locations}
+          setLocations={setLocations}
+        />
       </header>
     </div>
   );
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,167 +1,178 @@
-import React, { useRef } from "react";
-import "./Form.style.css";
+import React from "react";
 
-export const Form = () => {
-  const nameRef = useRef();
-  const descriptionRef = useRef();
-  const locationRef = useRef();
-
-  const longitudeRef = useRef();
-  const latitudeRef = useRef();
-  const continentRef = useRef();
-  const countryRef = useRef();
-
+export const Form = ({
+  levels,
+  setLevels,
+  setName,
+  setDescription,
+  setLongitude,
+  setLatitude,
+  setContinent,
+  setCountry,
+  locations,
+  setLocations,
+}) => {
+  // Add rendering Table after adding record - same for Locations
   const addLevels = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://0.0.0.0:13899/api/levels", {
+    const response = await fetch("https://strapi-km.herokuapp.com/api/levels", {
       headers: {
-        Authorization: `Bearer {${process.env.API_KEY}}`,
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
         "Content-Type": "application/json",
       },
       method: "POST",
-
       body: JSON.stringify({
         data: {
-          name: nameRef.current.value,
-          description: descriptionRef.current.value,
-          location: locationRef.current.value,
+          name: e.target.name.value,
+          description: e.target.description.value,
         },
       }),
     });
+    const data = await response.json();
+    const newData = data.data;
+    setLevels([...levels, newData]);
+    e.target.reset();
   };
 
   const addLocations = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://0.0.0.0:13899/api/locations", {
-      headers: {
-        Authorization: `Bearer {${process.env.API_KEY}}`,
-        "Content-Type": "application/json",
-      },
-      method: "POST",
-      body: JSON.stringify({
-        data: {
-          longitude: longitudeRef.current.value,
-          latitude: latitudeRef.current.value,
-          continent: continentRef.current.value,
-          country: countryRef.current.value,
+    const response = await fetch(
+      "https://strapi-km.herokuapp.com/api/locations",
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`,
+          "Content-Type": "application/json",
         },
-      }),
-    });
+        method: "POST",
+        body: JSON.stringify({
+          data: {
+            longitude: locations.longitude,
+            latitude: locations.latitude,
+            continent: locations.continent,
+            country: locations.country,
+          },
+        }),
+      }
+    );
+    const data = await response.json();
+    const newData = data.data;
+    setLocations([...locations, newData]);
+    e.target.reset();
   };
 
   return (
-    <div className="columns">
-      <div className="column">
-        <form onSubmit={addLevels}>
-          <h1 className="title is-2 is-spaced has-text-white mt-5">
-            Levels Data
-          </h1>
+    <>
+      <div className="columns">
+        <div className="column">
+          <form onSubmit={addLevels}>
+            <h1 className="title is-3 is-spaced has-text-white mt-1">Levels</h1>
 
-          <div className="field">
-            <label htmlFor="name">Name</label>
-            <div className="control">
-              <input
-                type="text"
-                id="name"
-                ref={nameRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
+            <div className="field">
+              <label htmlFor="name" className="label has-text-white">
+                Name
+              </label>
+              <div className="control">
+                <input
+                  type="text"
+                  id="name"
+                  onChange={(e) => setName(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
             </div>
-          </div>
 
-          <div className="field">
-            <label htmlFor="description">Description</label>
-            <div className="control">
-              <input
-                id="description"
-                ref={descriptionRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
+            <div className="field">
+              <label htmlFor="description" className="label has-text-white">
+                Description
+              </label>
+              <div className="control">
+                <input
+                  id="description"
+                  onChange={(e) => setDescription(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
             </div>
-          </div>
 
-          <div className="field">
-            <label htmlFor="location">Location</label>
             <div className="control">
-              <input
-                id="location"
-                ref={locationRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
+              <button type="submit" className="button is-primary">
+                Submit
+              </button>
             </div>
-          </div>
+          </form>
+        </div>
 
-          <div className="control">
-            <button type="submit" className="button is-large is-primary">
-              Submit
-            </button>
-          </div>
-        </form>
+        <div className="column">
+          <form onSubmit={addLocations}>
+            <h1 className="title is-3 is-spaced has-text-white mt-1">
+              Location
+            </h1>
+
+            <div className="field">
+              <label htmlFor="longitude" className="label has-text-white">
+                Longitude
+              </label>
+              <div className="control">
+                <input
+                  id="longitude"
+                  onChange={(e) => setLongitude(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
+            </div>
+
+            <div className="field">
+              <label htmlFor="latitude" className="label has-text-white">
+                Latitude
+              </label>
+              <div className="control">
+                <input
+                  id="latitude"
+                  onChange={(e) => setLatitude(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
+            </div>
+
+            <div className="field">
+              <label htmlFor="continent" className="label has-text-white">
+                Continent
+              </label>
+              <div className="control">
+                <input
+                  id="continent"
+                  onChange={(e) => setContinent(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
+            </div>
+
+            <div className="field">
+              <label htmlFor="country" className="label has-text-white">
+                Country
+              </label>
+              <div className="control">
+                <input
+                  id="country"
+                  onChange={(e) => setCountry(e.target.value)}
+                  className="input is-primary is-small"
+                  autoComplete="off"
+                ></input>
+              </div>
+            </div>
+            <div className="control">
+              <button type="submit" className="button is-primary">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-
-      <div className="column">
-        <form onSubmit={addLocations}>
-          <h1 className="title is-2 is-spaced has-text-white mt-5">
-            Location Data
-          </h1>
-
-          <div className="field">
-            <label htmlFor="longitude">Longitude</label>
-            <div className="control">
-              <input
-                id="longitude"
-                ref={longitudeRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
-            </div>
-          </div>
-
-          <div className="field">
-            <label htmlFor="latitude">Latitude</label>
-            <div className="control">
-              <input
-                id="latitude"
-                ref={latitudeRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
-            </div>
-          </div>
-
-          <div className="field">
-            <label htmlFor="continent">Continent</label>
-            <div className="control">
-              <input
-                id="continent"
-                ref={continentRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
-            </div>
-          </div>
-
-          <div className="field">
-            <label htmlFor="country">Country</label>
-            <div className="control">
-              <input
-                id="country"
-                ref={countryRef}
-                className="input is-primary is-medium"
-                autoComplete="off"
-              ></input>
-            </div>
-          </div>
-          <div className="control">
-            <button type="submit" className="button is-large is-primary">
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+    </>
   );
 };
