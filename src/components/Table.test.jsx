95f5c30f90812460c944b1c6a21c689e2bb92be3
Maciek <maciej.zarakowski@gmail.com>
2023-06-14 15:42:32 +0200

    updated Table.test

--- src/components/Table.test.jsx
+++ src/components/Table.test.jsx
@@ -1,28 +1,29 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { Table } from "./Table";
-import Api from "services/Api";
 
-jest.mock("../services/Api");
+const axios = require("axios");
+
+jest.mock("axios");
 
 describe("Table", () => {
   it("if mocked Level Table is fetched", async () => {
-    const mockedLevels = {
+    let mockedLevels = {
       data: [
         { id: 1, attributes: { name: "Europa", description: "opis Europa" } },
         { id: 2, attributes: { name: "Ameryka", description: "opis Ameryka" } },
       ],
     };
 
-    Api.instance = jest.fn().mockReturnValue({
-      AxiosGetLevels: jest.fn().mockResolvedValue(mockedLevels),
-    });
+    axios.get.mockResolvedValue(mockedLevels);
 
     render(
       <MemoryRouter>
         <Table
-          levels={mockedLevels}
-          setLevels={() => {}}
+          levels={mockedLevels.data}
+          setLevels={(levels) => {
+            mockedLevels = levels;
+          }}
           locations={[]}
           setLocations={() => {}}
         />
